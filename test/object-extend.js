e78034d031b256c41d03cdd41005335c04e73590
Nick Meldrum <nick@nickmeldrum.com>
2015-04-21 20:37:33 +0100

    added some test coverage

--- test/object-extend.js
+++ test/object-extend.js
@@ -9,5 +9,22 @@ describe('#extend', function() {
 
         original.newProp.should.equal('oh hai');
     });
+
+    it('maintains a property that was already on the object', function() {
+        var original = { "oldProp": "hello world" };
+
+        extend.call(original, { "newProp": "oh hai" });
+
+        original.oldProp.should.equal('hello world');
+        original.newProp.should.equal('oh hai');
+    });
+
+    it('replaces a property on an object', function() {
+        var original = { "prop": "hello world" };
+
+        extend.call(original, { "prop": "oh hai" });
+
+        original.prop.should.equal('oh hai');
+    });
 });
 
